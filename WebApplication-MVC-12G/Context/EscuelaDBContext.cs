886ab36b069420dd786131c23f7e0c8319da8f0b
christian.slapak <christian.slapak@ort.edu.ar>
2023-05-29 20:39:36 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication-MVC-12G/Context/EscuelaDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication_MVC_12G.Models;
+
+namespace WebApplication_MVC_12G.Context
+{
+    public class EscuelaDBContext : DbContext
+    {
+        public EscuelaDBContext(DbContextOptions<EscuelaDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
